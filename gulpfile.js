6bc29b4f37064161cdd925f5e57a53c40fcd6a91
Daniel Pacak <pacak.daniel@gmail.com>
2015-09-06 16:17:01 +0200

    chore(gulp): distribute scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,46 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var prettify = require('gulp-jsbeautifier');
 var wrap = require('gulp-wrap');
+var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 
 var paths = {
     scripts: [
         'src/sc-embedded-database.module.js',
         'src/sc-embedded-database.factory.js'
-    ]
+    ],
+    dist: 'dist'
 };
 
+var version = '0.0.4-build';
+
 gulp.task('clean', function () {
-    return del(['dist']);
+    return del([paths.dist]);
 });
 
-gulp.task('scripts', ['clean'], function () {
+gulp.task('scripts', function () {
     return gulp.src(paths.scripts)
         .pipe(concat('sc-embedded-database.js'))
-        .pipe(wrap({src: 'gulp/wrap.template.scripts'}, {version: '0.0.4-build'}))
-        .pipe(gulp.dest('dist'));
+        .pipe(wrap({src: 'gulp/wrap.template.iife'}))
+        .pipe(prettify({indentSize: 3}))
+        .pipe(wrap({src: 'gulp/wrap.template.banner'}, {version: version}))
+        .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('default', ['scripts']);
+// FIXME The gulp-wrap plugin is not compatible with gulp-sourcemaps. Consider using gulp-wrap-js.
+gulp.task('scripts.min', function () {
+    return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
+        .pipe(concat('sc-embedded-database.min.js'))
+        .pipe(wrap({src: 'gulp/wrap.template.iife'}))
+        .pipe(uglify())
+        .pipe(wrap({src: 'gulp/wrap.template.banner'}, {version: version}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.dist))
+});
+
+gulp.task('dist', ['clean', 'scripts', 'scripts.min']);
+
+gulp.task('default', ['dist']);
